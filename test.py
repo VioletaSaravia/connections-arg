21dd6e2792f150ad8d741681820c1cecd039fd8a
Violeta Saravia <saraviavioleta@gmail.com>
2024-12-06 01:23:30 -0300

    first commit

---
+++ test.py
@@ -0,0 +1,47 @@
+import matplotlib
+matplotlib.use('tkAgg')
+import matplotlib.pyplot as plt
+import networkx as nx
+import json
+
+with open("conecciones.json", 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+# LOAD NODES
+G = nx.Graph()
+
+for k, v in data.items():
+    for n in range(len(v) - 1):
+        G.add_edge(v[n], v[n + 1])
+    G.add_edge(v[-1], v[0])
+
+pos = nx.spring_layout(G)
+nx.draw_networkx(G, pos)
+
+# LOAD EDGES
+pos_cycles = {t: [pos[x] for x in data[t]] for t in data.keys()}
+pos_cycles_centers = {t: [0, 0] for t in data.keys()}
+
+for k, v in pos_cycles.items():
+    for n in v:
+        pos_cycles_centers[k][0] += n[0]  
+        pos_cycles_centers[k][1] += n[1]  
+
+    # TODO This is the average. We may want the mean.
+    pos_cycles_centers[k][0] /= len(v)
+    pos_cycles_centers[k][1] /= len(v) 
+
+E = nx.Graph()
+
+for n in pos_cycles_centers.keys():
+    E.add_node(n)
+
+nx.draw_networkx(
+    E,
+    pos=pos_cycles_centers,
+    **{
+        'node_color':'red'
+    }
+)
+
+plt.show()
